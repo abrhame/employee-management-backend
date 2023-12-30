ed6d41f34079cf8d32e2f072cd5a6c0ea1301faf
abrhame <ablakew7ab@gmail.com>
2023-12-30 10:25:40 +0300

    feat: add Employee model

---
+++ Model/Employee.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const employeeSchema = mongoose.Schema({
+    name: {
+        type:String,
+        required:true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    phoneNumber: {
+        type: String,
+        required:true,
+        unique:true,
+    },
+    department: {
+        type:String,
+        required:true
+    },
+    role: {
+        type:String,
+        required:true,
+    }
+})
--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 const express = require('express');
 require('dotenv').config();
+const mongoose = require('mongoose')
 
 
 const app = express();
 const port = process.env.PORT || 3000;
+const uri = process.env.DB_CONNECTION_STRING;
+
+mongoose.connect(uri, {useNewUrlParser:true, useUnifiedTopology:true});
+
+const db = mongoose.connection;
+
+db.on("connected", () => {
+    console.log("Cnnected to Database");
+})
+
+db.on("error", (error) => {
+    console.log("error: ", error );
+})
+
+db.on("disconnected", () => {
+    console.log("Disconnected from Databaase");
+})
+
+db.on("SIGINT", () => {
+    mongoose.connection.close(() => {
+        console.log("Mongodb connection closed through app termination");
+        process.exit(0);
+    });
+});
+
+
+
 
 app.get('/',(req,res) => {
     res.send("Hello world")
