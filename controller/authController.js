55ecfe7bed8f94cc5257856c008463dd3d3c9c50
abrhame <ablakew7ab@gmail.com>
2024-01-03 14:36:01 +0300

    feat: add token validataor

--- controller/authController.js
+++ controller/authController.js
@@ -45,6 +45,21 @@ const authController = {
         } catch(error) {
             res.status(500).json(`Internal Server Error: ${error}`);
         }
+    },
+
+    validateToken: async (req,res, next) => {
+        const authHeader = req.headers['authorization'];
+        const token = authHeader && authHeader.split(' ')[1];
+
+        if (token == null){
+            return res.sendStatus(401);
+        }
+
+        jwt.verify(token, process.env.JWT_SECRET_KEY, (err, user) => {
+            if (err) return res.sendStatus(403);
+            req.user = user;
+            next();
+        })
     }
 }
 
